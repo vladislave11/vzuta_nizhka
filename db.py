6746d6865f222e7c1627dac1ebfb03f8ca3e8e31
Vadim <vadimpapusha2310@gmail.com>
2024-03-10 19:58:50 +0200

    створив бд і написав функцію для форми, яка динамічно додає нове взуття в бд

---
+++ db.py
@@ -0,0 +1,30 @@
+import sqlite3
+
+
+
+try:
+    sqlite_connection = sqlite3.connect("vzuta.db")
+    sqlite_create_table_query = '''  CREATE TABLE IF NOT EXISTS shoes(
+                                    name TEXT,
+                                    price INTEGER,
+                                    description TEXT,
+                                    photo1 TEXT,
+                                    photo2 TEXT,
+                                    photo3 TEXT,
+                                    photo4 TEXT,
+                                    photo5 TEXT,
+                                    photo6 TEXT,
+                                    photo7 TEXT); '''
+    cursor = sqlite_connection.cursor()
+    cursor.execute(sqlite_create_table_query)
+    sqlite_connection.commit()
+    print('Table ORDERS Created')
+    cursor.close()
+
+except sqlite3.Error as error:
+    print('Error: ', error)
+
+finally:
+    if sqlite_connection:
+        sqlite_connection.commit()
+        print('DB disconnected')
--- main.py
+++ main.py
@@ -1,10 +1,41 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, abort, url_for
+import sqlite3
 
 app = Flask(__name__)
+
+
+def get_db_connection():
+    connection = sqlite3.connect(database='vzuta.db')
+    connection.row_factory = sqlite3.Row
+    return connection
 
 @app.route('/')
 def main():
     return render_template('index.html')
+
+@app.route('/add', methods = ['GET', 'POST']) #Для того, щоб можна було заповнювати магазин товаром динамічно через форму
+def add_model():
+    if request.method == 'POST':
+        name = request.form['name']
+        description = request.form['description']
+        price = request.form['price']
+        photo1 = request.form['photo1']
+        photo2 = request.form['photo2']
+        photo3 = request.form['photo3']
+        photo4 = request.form['photo4']
+        photo5 = request.form['photo5']
+        photo6 = request.form['photo6']
+        photo7 = request.form['photo7']
+        connection = get_db_connection()
+        connection.execute("INSERT INTO vzuta.db (name,price,description,photo1,photo2,photo3,photo4,photo5,photo6,photo7) VALUES (?,?,?,?,?,?,?,?,?,?)", (name,price,description,photo1,photo2,photo3,photo4,photo5,photo6,photo7))
+        connection.commit()
+        connection.close()
+        return render_template("index.html")
+
+    return render_template('add_model_form.html')
+
+
+#тобі потрібно написати форму в файлі add_model_form 👆
 
 
 
