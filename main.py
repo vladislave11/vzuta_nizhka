31593944f329c6e7c2363d7b4f652d71b4f890b8
Vadim <vadimpapusha2310@gmail.com>
2024-03-14 17:37:18 +0200

    тепер ти можеш заповнювати магазин взуттям за зразком, який я написав у індекс.хтмл

--- main.py
+++ main.py
@@ -9,19 +9,62 @@ def get_db_connection():
     connection.row_factory = sqlite3.Row
     return connection
 
-@app.route('/')
-def main():
-    connection = get_db_connection()
-    shoes = connection.execute('SELECT * FROM shoes')
-
-    return render_template('index.html',
-                           shoe = shoes)
-
-
 connection = get_db_connection()
 shoe = connection.execute('SELECT * FROM shoes')
+shoes = {}
+names = []
+prices = []
+descrips = []
+photo1s = []
+photo2s = []
+photo3s = []
+photo4s = []
+photo5s = []
+photo6s = []
+photo7s = []
+
+
+
 for i in shoe:
-    print(i['name'])
+    names.append(i['name'])
+    shoes['name'] = names
+    prices.append(i['price'])
+    shoes['price'] = prices
+    descrips.append(i['description'])
+    shoes['descriptions'] = descrips
+    photo1s.append(i['photo1'])
+    shoes['photo1'] = photo1s
+    photo2s.append(i['photo2'])
+    shoes['photo2'] = photo2s
+    photo3s.append(i['photo3'])
+    shoes['photo3'] = photo3s
+    photo4s.append(i['photo4'])
+    shoes['photo4'] = photo4s
+    photo5s.append(i['photo5'])
+    shoes['photo5'] = photo5s
+    photo6s.append(i['photo6'])
+    shoes['photo6'] = photo6s
+    photo7s.append(i['photo7'])
+    shoes['photo7'] = photo7s
+
+
+@app.route('/')
+def main():
+    length = len(shoes['name'])
+    return render_template('index.html', shoes = shoes, length = length)
+
+
+
+
+
+print(shoes)
+
+
+
+
+
+
+
 
 
 @app.route('/add', methods = ['GET', 'POST'])
